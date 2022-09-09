06ae0655a0d3913557263fb8774549e9d659400a
GlauciaBierwagen <galsilv@alumni.usp.br>
2022-09-09 17:13:37 +0200

    Boxes-main

--- boxes-main/script.js
+++ boxes-main/script.js
@@ -54,5 +54,25 @@ boxes.forEach((box) => {
   });
 });
 
+function openModal(mn) {
+  let modal = document.getElementById(mn);
+
+  if (typeof modal == 'undefined' || modal === null)
+      return;
+
+  modal.style.display = 'Block';
+  document.body.style.overflow = 'hidden';
+}
+
+function closeModal(mn) {
+  let modal = document.getElementById(mn);
+
+  if (typeof modal == 'undefined' || modal === null)
+      return;
+
+  modal.style.display = 'none';
+  document.body.style.overflow = 'auto';
+}
+
 
 
