8455fcc274f47f10b555a2df5ff7a307358b0508
Gláucia Silva Bierwagen <98116061+glauciabierwagen@users.noreply.github.com>
2025-12-04 17:05:25 +0100

    Update Js

--- js.js
+++ js.js
@@ -29,3 +29,38 @@ $(document).ready(function () {
     )
   })
 })
+
+const menuIcon = document.querySelector(".hamburger-menu");
+ const navbar = document.querySelector(".navbar");
+
+ menuIcon.addEventListener("click", () => {
+    navbar.classList.toggle("change");
+ });
+
+const slideshow = document.querySelector('.slideshow')
+
+setInterval(() => {
+
+    const firstIcon = slideshow.firstElementChild
+
+    firstIcon.classList.add('faded-out')
+
+    const thirdIcon = slideshow.children[3]
+
+    thirdIcon.classList.add('light')
+
+    thirdIcon.previousElementSibling.classList.remove('light')
+
+    setTimeout(() => {
+        slideshow.removeChild(firstIcon)
+
+        slideshow.appendChild(firstIcon)
+
+        setTimeout(() => {
+            firstIcon.classList.remove('faded-out')
+        }, 500)
+        
+    }, 500)
+    
+
+}, 3000)
