ac089887569766e80353f08bbb93e31422ec7511
Gláucia Silva Bierwagen <98116061+glauciabierwagen@users.noreply.github.com>
2025-12-09 11:44:12 +0100

    Updates

--- js.js
+++ js.js
@@ -5,16 +5,3 @@
     navbar.classList.toggle("change");
  });
 
- 
-  $('a[href*="#"]').on("click", function (e) {
-    e.preventDefault()
-
-    $("html, body").animate(
-      {
-        scrollTop: $($(this).attr("href")).offset().top,
-      },
-      500,
-      "linear"
-    )
-  })
-
