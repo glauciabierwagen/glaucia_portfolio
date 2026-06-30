1613fa93ae2e57e9c91020f0d0050ef43ecf1e32
Gláucia Silva Bierwagen <98116061+glauciabierwagen@users.noreply.github.com>
2026-06-30 19:34:52 +0200

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,15 @@
 // Footer.jsx - This component renders the footer of the page.
 // It shows social media icons that link to external profiles.
-
+import '../../styles/footer.css'
 // socialLinks is an array of objects. Each object holds:
 // - href: the URL to open when clicked
 // - icon: the Font Awesome class for the icon
 // - label: a text description used for accessibility (aria-label)
 const socialLinks = [
-  { href: 'https://github.com/glauciabierwagen',                                      icon: 'fab fa-github',    label: 'Github' },
-  { href: 'https://www.facebook.com/glaucia.silva.121',                               icon: 'fab fa-facebook-f', label: 'Facebook' },
+  { href: 'https://github.com/glauciabierwagen',                                      icon: 'fab fa-github',      label: 'Github' },
   { href: 'https://www.linkedin.com/in/gl%C3%A1ucia-silva-bierwagen-09860420/',       icon: 'fab fa-linkedin-in', label: 'LinkedIn' },
-  { href: 'https://www.instagram.com/galbierwagen/',                                   icon: 'fab fa-instagram',  label: 'Instagram' },
+  { href: 'https://www.instagram.com/galbierwagen/',                                   icon: 'fab fa-instagram',   label: 'Instagram' },
+  { href: 'https://www.facebook.com/glaucia.silva.121',                               icon: 'fab fa-facebook-f',  label: 'Facebook' },
 ]
 
 export default function Footer() {
